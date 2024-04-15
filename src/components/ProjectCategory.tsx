85851fdbe4f6114c2ed107b72450ed2e70f0f646
codebysid <codebysidd@gmail.com>
2024-04-16 03:42:15 +0530

    type

--- src/components/ProjectCategory.tsx
+++ src/components/ProjectCategory.tsx
@@ -6,7 +6,7 @@ import { Button } from './ui/button'
 const ProjectCategory = ({ category, data }: { category: string, data: any }) => {
   return (
     <div className='flex flex-col md:w-2/6 lg:w-2/6 gap-2'>{
-      data?.filter((ele: any) => ele.role.toLowerCase() === category.toLowerCase()).map((ele: any, key) => {
+      data?.filter((ele: any) => ele.role.toLowerCase() === category.toLowerCase()).map((ele: any, key: number) => {
         if (key === 3) return
         return <ProjectCard key={key} _id={ele._id} title={ele.title} description={ele.description} role={ele.role} theme={ele.theme} />
       })
